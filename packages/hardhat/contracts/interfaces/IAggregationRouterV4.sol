f67dcb1e45baaabf077c83b44dfdfd091fd9b08e
Kevin Huo <kevin.w.huo@gmail.com>
2022-06-25 01:14:26 -0400

    add skeleton for 1inch aggregator

---
+++ packages/hardhat/contracts/interfaces/IAggregationRouterV4.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IAggregagationExecutor} from "./IAggregagationExecutor.sol";
+
+/// @title Aggregagation Router
+interface IAggregagationRouterV4 {
+    struct SwapDescription {
+        IERC20 srcToken;
+        IERC20 dstToken;
+        address payable srcReceiver;
+        address payable dstReceiver;
+        uint256 amount;
+        uint256 minReturnAmount;
+        uint256 flags;
+        bytes permit;
+    }
+
+    /// @notice Peforms swap
+    /// @param caller Caller
+    /// @param desc Swap description
+    function swap(
+        IAggregationExecutor caller,
+        SwapDescription calldata desc,
+        bytes calldata data
+    )
+        external
+        payable
+        returns (
+            uint256 returnAmount,
+            uint256 spentAmount,
+            uint256 gasLeft
+        );
+}
---
+++ packages/hardhat/contracts/libraries/CommonErrors.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+library CommonErrors {
+    error ZeroAddress();
+    error NotAuthorized();
+    error ZeroAmount();
+    error ZeroSlippage();
+    error IncorrectSwapInformation();
+}
